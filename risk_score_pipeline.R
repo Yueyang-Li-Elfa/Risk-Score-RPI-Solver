85cd21903ea2e98e4ef89474701121b1a66f5a3e
Yueyang Li <elfa.l@outlook.com>
2020-06-28 21:34:32 -0400

    Update

--- risk_score_pipeline.R
+++ risk_score_pipeline.R
@@ -4,26 +4,29 @@
 # please make sure to set the [Working Directory] as where the script is located
 
 # How to use this script
-# 1. Clone the whole repository from GitHub.
-# 2. Download the latest 3 weeks [Weekly Places Patterns v2] data from SafeGraph, store in the [Data] folder, and unzip the files.
-# 3. Download COVID-19 data from :http://dashboard.publichealth.lacounty.gov/covid19_surveillance_dashboard/
+# 1. Clone the whole repository from GitHub
+# 2. Download the latest 3 weeks [Weekly Places Patterns v2] data from SafeGraph, store in the [Data] folder, and unzip the files
+# 3. Download COVID-19 data from: http://dashboard.publichealth.lacounty.gov/covid19_surveillance_dashboard/
 #    Click [City/Community Table], then click [Download this table]
 #    Store this csv file in the [Data] folder
 # 4. Set up the three dates in [line 24-26] as "mm-dd" format
+# 5. Run through the scirpt. The result will be stored in the [Risk Score] folder.
 
 library(readr)
 library(dplyr)
 library(purrr)
 library(stringr)
 library(tidyr)
+library(sjmisc)
 
 # ====================================================
-# Weekly Pattern: Subset POIs in LA
+# Weekly Pattern: Subset POIs in LA 
+# Estimated runtime: 45 mins
 # ----------------------------------------------------
 
-date1 <- "05-25"
-date2 <- "06-01"
-date3 <- "06-08"
+date1 <- "06-01"
+date2 <- "06-08"
+date3 <- "06-15"
 
 # Week1
 weekly1 <- read_csv(str_c("Data/2020-", date1, "-weekly-patterns.csv"))
@@ -42,11 +45,13 @@ weekly3 <- read_csv(str_c("Data/2020-", date3, "-weekly-patterns.csv"))
 weekly3_LA <- weekly3 %>%
   filter(region == "CA", city == "Los Angeles")
 rm(weekly3)
+write_csv(weekly3_LA, "Data/weekly_0615_LA.csv")
 
 # ====================================================
 
 # ====================================================
 # Weekly Pattern: Parse the data and take average for last three weeks
+# Estimated runtime: 10 mins
 # ----------------------------------------------------
 
 get_daily_visits <- function (x) {
@@ -83,19 +88,19 @@ get_hourly_visits <- function(x) {
   temp_hourly
 }
 
-daily <- get_daily_visits(weekly_0525_LA) %>%
+daily <- get_daily_visits(weekly1_LA) %>%
   # bind three datasets together
-  rbind(get_daily_visits(weekly_0601_LA)) %>%
-  rbind(get_daily_visits(weekly_0608_LA)) %>%
+  rbind(get_daily_visits(weekly2_LA)) %>%
+  rbind(get_daily_visits(weekly3_LA)) %>%
   # take average of last three weeks
   group_by(safegraph_place_id, weekday) %>%
   summarize(avg_visits = mean(visits_by_day, na.rm = T),
             avg_median_dwell = mean(median_dwell, na.rm = T))
 
-hourly <- get_hourly_visits(weekly_0525_LA) %>%
+hourly <- get_hourly_visits(weekly1_LA) %>%
   # bind 3 datasets together
-  rbind(get_hourly_visits(weekly_0601_LA)) %>%
-  rbind(get_hourly_visits(weekly_0608_LA)) %>%
+  rbind(get_hourly_visits(weekly2_LA)) %>%
+  rbind(get_hourly_visits(weekly3_LA)) %>%
   # calculate cv & peak visits for each week
   group_by(safegraph_place_id, weekday, date_range_start) %>%
   summarise(sd = sd(visits_by_each_hour),
@@ -112,8 +117,47 @@ hourly <- get_hourly_visits(weekly_0525_LA) %>%
 
 # ====================================================
 # Infection Rate: Calculate the infection rate for each community
+# Estimated runtime: 15 mins
 # ----------------------------------------------------
-# Missing
+
+# Calculate infection rate for each community
+rate_original <- read_csv("Data/city_community_table.csv")
+rate <- rate_original %>%
+  mutate(infection_rate = (cases_final-deaths_final)/(persons_tested_final-deaths_final)) %>%
+  select(geo_merge, infection_rate)
+
+# Read in poi data
+poi <- read_csv("Data/poi_extended.csv")
+
+# Add infection rate for each poi
+poi$infection_rate <- 0
+
+for (i in 1:nrow(poi)) {
+  city=poi$city[i]
+  comm=poi$community[i]
+  for (j in 1:nrow(rate)){
+    if(str_contains(rate$geo_merge[j], comm,ignore.case = TRUE)==TRUE) {
+      poi$infection_rate[i]=rate$infection_rate[j]
+    }
+    else if(str_contains(rate$geo_merge[j], city,ignore.case = TRUE)==TRUE & city!="Los Angeles") {
+      poi$infection_rate[i]=rate$infection_rate[j]
+    }
+  }
+}
+
+# Impute rest pois (those that can't find matches) with the infection rate of LA city
+rate_LA <- rate_original %>%
+  filter(str_detect(geo_merge, "^Los Angeles")) %>%
+  summarise(cases_final = sum(cases_final),
+            deaths_final = sum(deaths_final),
+            persons_tested_final = sum(persons_tested_final)) %>%
+  mutate(infection_rate = (cases_final-deaths_final)/(persons_tested_final-deaths_final))
+
+rate_LA <- rate_LA$infection_rate
+
+poi <- poi %>%
+  mutate(infection_rate = ifelse(infection_rate == 0, rate_LA, infection_rate))
+
 # ====================================================
 
 # ====================================================
@@ -122,15 +166,14 @@ hourly <- get_hourly_visits(weekly_0525_LA) %>%
 
 # Read in data
 # POI / open hours
-rate <- read_csv("infection_rate/POI_comm_rates_0624.csv")   # will be eliminated later
-poi <- read_csv("SafeGraph/core_places/poi_extended.csv")
-open_hours <- read_csv("SafeGraph/core_places/poi_hour_imputed.csv")
+poi <- read_csv("Data/poi_extended.csv")
+open_hours <- read_csv("Data/poi_hour_imputed.csv")
 
 # Joining the data
 # (will be modified after we include the infection rate data in this script)
 risk <- open_hours %>%
   # Join the poi data
-  left_join(poi %>% select(safegraph_place_id, location_name, top_category, latitude, longitude, street_address, area_square_feet), 
+  left_join(poi %>% select(safegraph_place_id, location_name, top_category, latitude, longitude, street_address, area_square_feet, community, city, infection_rate), 
             by = c("safegraph_place_id")) %>%
   # Join daily visits data
   # For those with open_hours = 0 but still have visitis, adjust the open_hours to median level
@@ -139,9 +182,6 @@ risk <- open_hours %>%
   mutate(open_hours = ifelse(open_hours == 0 & avg_visits != 0,
                              median(open_hours), open_hours)) %>%
   ungroup() %>%
-  # Join infection rate data
-  left_join(rate %>% select(safegraph_id, `infection%`), by = c("safegraph_place_id" = "safegraph_id")) %>%
-  rename(infection_rate = `infection%`) %>%
   # Join the hourly visit data
   left_join(hourly, by = c("safegraph_place_id", "weekday"))
 
@@ -163,8 +203,7 @@ for (i in 1:nrow(risk)) {
   prob[i] <- ifelse(size == 0, 0,
                     sum(dbinom(1:size, size, p)))
 }
-# Summary
-#summary(prob)
+
 # Add to the risk table
 risk$prob <- prob
 
@@ -193,7 +232,7 @@ risk <- risk %>%
 
 # Reorder columns
 risk <- risk %>%
-  select(safegraph_place_id, location_name, top_category, latitude, longitude, street_address, everything())
+  select(safegraph_place_id, location_name, top_category, latitude, longitude, street_address, community, city, everything())
 
 # Save the file
 write_csv(risk, str_c("Risk Score/risk_score_", Sys.Date(), "updated.csv"))
