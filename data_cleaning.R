097f57b0f0102112da3250e00e64566c0c0ecfe3
Yueyang Li <elfa.l@outlook.com>
2020-07-28 17:59:33 -0400

    Update data cleaning script
    Add annotation. Add imputing open hours.

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,4 +1,5 @@
 # Data Cleaning script
+# To run in your environment, don't forget to change the path included in the script
 
 library(readr)
 library(dplyr)
@@ -43,3 +44,29 @@ core_poi_LA <- core_poi_LA %>%
 
 # Save the file
 write_csv(core_poi_LA, path = "Data/poi_extended.csv")
+
+# Impute open hours
+# Please find script POI_location.ipynb
+# Read in the output file here
+open_hours <- read_csv("SafeGraph/core_places/poi_hour.csv") 
+
+# Impute open hours, using median value
+# when calculating median, exclude 0s
+# IF this poi is noy closed this day, it will open for ... hours.
+impute_median <- function (x) {
+  value <- median(x[x != 0], na.rm = T)
+  for (i in 1:length(x)) {
+    if (is.na(x[i])) {
+      x[i] <- value
+    }
+  }
+  return(x)
+}
+open_hours[, 2:8] <- map_dfr(open_hours[, 2:8], impute_median)
+
+# Gather the open hours data to a tidy dataframe
+open_hours <- open_hours %>% 
+  gather("weekday", "open_hours", -safegraph_place_id)
+
+# Save the file
+write_csv(open_hours, path = "Data/poi_hour_imputed.csv")
